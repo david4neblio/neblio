635e83c057b139155dbbc0ff8a71636f1d7df554
david4neblio <43017430+david4neblio@users.noreply.github.com>
2019-04-17 13:52:01 -0400

    Removed extra build settings

--- ci_scripts/test_rpi-daemon-gui.py
+++ ci_scripts/test_rpi-daemon-gui.py
@@ -41,26 +41,4 @@ nci.call_with_err_code('mv ' + os.path.join(deploy_dir,'.ccache', '') + ' ' + os
 
 file_name = '$(date +%Y-%m-%d)---' + os.environ['TRAVIS_BRANCH'] + '-' + os.environ['TRAVIS_COMMIT'][:7] + '---' + build_target_alt + '---RPi-raspbian-stretch.tar.gz'
 
-# since RPi jobs are run several times we need to be creative in our deployment as only the original job can post to GitHub Releases.
-# So, start the original job via the GitHub tag and pass its job ID to every subsequent job. Once the build is complete, the final build job
-# will restart the original job, which will handle the deployment to GitHub releases.
-deploy_job_id = os.environ.get('TRAVIS_DEPLOY_JOB_ID', '0')
-travis_tag    = os.environ.get('TRAVIS_TAG', '')
-# if travis tag is populated, this is the job ID we want
-if (travis_tag != ''):
-  deploy_job_id = os.environ['TRAVIS_JOB_ID']
-
-# Check if binary exists before trying to package it.
-# If it does not exist we had a build timeout
-if(os.path.isfile(build_target)):
-  nci.call_with_err_code('tar -zcvf "' + file_name + '" ' + build_target)
-  nci.call_with_err_code('rm -f ' + build_target)
-  nci.call_with_err_code('echo "Binary package at ' + deploy_dir + file_name + '"')
-
-  if (travis_tag == '' and deploy_job_id != '0'):
-  	nci.call_with_err_code('echo "Restarting our deploy job. Job ID: ' + deploy_job_id + '"')
-  	nci.call_with_err_code('curl -X POST -H "Content-Type: application/json" -H "Travis-API-Version: 3" -H "Accept: application/json" -H "Authorization: token ' + os.environ["TRAVIS_API_TOKEN"] + '" -d \'{}\' \'https://api.travis-ci.org/job/' + deploy_job_id + '/restart\'')
-
-else:
-  nci.call_with_err_code('echo "Binary not found, likely due to a timeout, starting new job..."')
-  nci.call_with_err_code('curl -X POST -H "Content-Type: application/json" -H "Travis-API-Version: 3" -H "Accept: application/json" -H "Authorization: token ' + os.environ["TRAVIS_API_TOKEN"] + '" -d \'{"request":{"message":"RPi ' + build_target_alt + ' Build Restart","branch":"' + os.environ["TRAVIS_BRANCH"] + '","config":{"merge_mode":"deep_merge","env":{"global":{"TRAVIS_DEPLOY_JOB_ID":"' + deploy_job_id + '"},"matrix":["target_v=' + os.environ["target_v"] + '"]}}}}\' \'https://api.travis-ci.org/repo/NeblioTeam%2Fneblio/requests\'')
+#Removed extra build settings as we are not posting anything to GitHub releases
